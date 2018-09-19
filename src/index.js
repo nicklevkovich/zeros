1e9fe47566f6ef974d2159f5beed90d5ccc88aae
Nick Levkovich <nicklevkovic@gmail.com>
2018-09-20 02:24:58 +0300

    Final solution - 100/100 passing. Too easy :D

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  zeros = 0;
+  while(number > 0) {
+    number = Math.floor(number/5);
+    zeros += number;
+  }
+  return(zeros);
 }
